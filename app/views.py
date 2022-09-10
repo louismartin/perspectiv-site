96691fa35324f5ed7ff93543d5c6b25a2c6a4c70
Louis Martin <louismartin@fb.com>
2022-09-10 23:25:43 +0100

    Don't display investments if no account connected

--- app/views.py
+++ app/views.py
@@ -7,8 +7,12 @@ from perspectiv.budget_insight_api import get_client_id, get_manage_connections_
 @login_required
 def app(request):
     df_investments = get_investments_df(request.user.budgetinsightuser.auth_token)
-    df_investments_html = df_investments.to_html(table_id="investments-table")
-    return render(request, "app/app.html", {"df_investments_html": df_investments_html})
+    # TODO: Display nothing when there are no accounts connected
+    context = {}
+    if df_investments is not None:
+        context["df_investments_html"] = df_investments.to_html(table_id="investments-table")
+        context["total_valuation"] = df_investments["valuation"].sum()
+    return render(request, "app/app.html", context)
 
 
 @login_required
