39dfdf71f65b5da03105df2226f345694e72de72
Louis Martin <louismartin@fb.com>
2022-09-10 23:06:51 +0100

    Fix wrong urls

--- website/urls.py
+++ website/urls.py
@@ -14,14 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
-from users import views as user_views
+from django.urls import path
 from django.contrib.auth import views as auth_views
+
+from users import views as user_views
+from app import views as app_views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("app.urls"), name="app"),
-    path("manage/", include("app.urls"), name="manage"),
+    path("", app_views.app, name="app"),
+    path("manage/", app_views.manage, name="manage"),
     path("register/", user_views.register, name="register"),
     path("profile/", user_views.profile, name="profile"),
     path("login/", auth_views.LoginView.as_view(template_name="users/login.html"), name="login"),
