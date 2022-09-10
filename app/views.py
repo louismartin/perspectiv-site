b311b9fd21ab64413bff078d0999b1db8de36c51
Louis Martin <louismartin@fb.com>
2022-09-10 20:32:28 +0100

    Display budget insight investments on the home page

--- app/views.py
+++ app/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, redirect
 
-from perspectiv.budget_insight_api import get_client_id, get_manage_connections_url
+from perspectiv.budget_insight_api import get_client_id, get_manage_connections_url, get_investments_df
 
 
 def app(request):
-    return render(request, "app/app.html")
+    df_investments = get_investments_df(request.user.budgetinsightuser.auth_token)
+    df_investments_html = df_investments.to_html(table_id="investments-table")
+    return render(request, "app/app.html", {"df_investments_html": df_investments_html})
 
 
 def manage(request):
